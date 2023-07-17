a6f779c4dcfe0fd56959f72265f0dcbbc8cc1263
duong.bd <duong.bd2000@vnpt.vn>
2023-07-18 02:17:24 +0700

    .

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-import * as cookieParser from 'cookie-parser';
 
 import { ValidationPipe } from '@nestjs/common';
 import { GlobalExceptionsFilter } from './middlewares/global-exceptions.filter';
@@ -9,7 +8,6 @@ import { GlobalExceptionsFilter } from './middlewares/global-exceptions.filter';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.use(cookieParser());
   const httpAdapter = app.get(HttpAdapterHost);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new GlobalExceptionsFilter(httpAdapter));
